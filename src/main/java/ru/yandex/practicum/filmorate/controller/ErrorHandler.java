be194d64e7000ec9a58962780b2599d0a926b8fb
Olesia Rumiantseva <pymanceva@yandex.ru>
2023-05-10 20:35:34 +0300

    feat: добавить Handler для MethodArgumentNotValidException

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,6 +21,11 @@ public class ErrorHandler {
     })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final ValidationException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
         return new ErrorResponse(e.getMessage());
     }
 
